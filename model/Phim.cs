c502855306e9e56037677eedbecfc6fdd18b16a8
LocPhamn <jasonpham20203@gmail.com>
2024-05-23 10:24:29 +0700

    update project

--- model/Phim.cs
+++ model/Phim.cs
@@ -10,6 +10,8 @@ namespace Project.model
     {
         private string id_phim {  get; set; }
         private string tenphim { get;set; }
+        private string nhan_phim { get; set; }
+
         private string quocgia { get; set; }
         private string theloai { get; set; }
         private int sove {  get; set; }
@@ -17,10 +19,11 @@ namespace Project.model
         private string hanglamphim { get; set; }
         private string ngayramat {  get; set; }
 
-        public Phim(string id_phim, string tenphim, string quocgia, string theloai, int sove, int thoiluong, string hanglamphim, string ngayramat)
+        public Phim(string id_phim, string tenphim,string nhan_phim, string quocgia, string theloai, int sove, int thoiluong, string hanglamphim, string ngayramat)
         {
             this.id_phim = id_phim;
             this.tenphim = tenphim;
+            this.nhan_phim = nhan_phim;
             this.quocgia = quocgia;
             this.theloai = theloai;
             this.sove = sove;
